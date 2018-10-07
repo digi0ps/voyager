52999d1be8fb736bfe83539a5f8e71d05818d7b0
jeremyphilemon <jeremyphilemon@outlook.com>
2018-10-06 20:24:01 -0400

    Updated footer image src.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,30 +1,37 @@
-import React, { Component } from 'react';
 import 'bulma/css/bulma.css';
 import './../App.css';
+
+import React, { Component } from 'react';
 
 class Footer extends Component {
   constructor(props) {
     super(props);
-    this.state={
-      visibility: false,
-    };
+    this.state = { visibility: false };
   }
 
   componentDidMount() {
-    let whitelist=['/fluff', '/', '/contact', ];
-    if(whitelist.includes(window.location.pathname)) {
-      this.setState({
-        visibility: true,
-      });
+    let whitelist = ['/fluff', '/', '/contact'];
+    if (whitelist.includes(window.location.pathname)) {
+      this.setState({ visibility: true });
     }
   }
 
   render() {
     return (
-      <footer className="footer is-unselectable whales-margin" style={{display: this.state.visibility ? 'inherit': 'none'}}>
+      <footer
+        className="footer is-unselectable whales-margin"
+        style={{
+          display: this.state.visibility ? 'inherit' : 'none'
+        }}
+      >
+        {' '}
         <div className="container">
           <div className="is-pulled-right">
-            <img alt="whale" className="whales is-pulled-right" src="https://storage.googleapis.com/abode/photos/whales.png" />
+            <img
+              alt="whale"
+              className="whales is-pulled-right"
+              src="https://storage.googleapis.com/jrmyphlmn/images/whales.png"
+            />
           </div>
         </div>
       </footer>
